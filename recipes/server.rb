de2132165c6e0ec1a118841632a6acd38f24c34d
David <david@cursochef.com>
2019-06-18 18:37:43 +0000

    Initial Commit

---
+++ recipes/server.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook:: myiis
+# Recipe:: server
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+powershell_script 'Install IIS' do
+  code 'Add-WindowsFeature Web-Server'
+end
+
+file 'C:\inetpub\wwwroot\Default.htm' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'w3svc' do
+  action [:enable, :start]
+end
